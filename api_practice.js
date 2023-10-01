e392588ffc31df9b3ada2db519f59ad8e1c7413c
Gabrielle Tirta <tirtagabrielle@gmail.com>
2023-10-01 23:29:34 +1100

    learn: location and histroy

--- api_practice.js
+++ api_practice.js
@@ -1,13 +1,17 @@
 // This is the examples of setTimeout
-function timeoutExample1(){
-    console.log("This messages id delayed 5 seconds");
-}
+// function timeoutExample1(){
+//     console.log("This messages id delayed 5 seconds");
+// }
 
-function timeoutExample2(){
-    console.log("This messages id delayed 1 second");
-}
+// function timeoutExample2(){
+//     console.log("This messages id delayed 1 second");
+// }
 
-setTimeout(timeoutExample1, 4000);
-setTimeout(timeoutExample2, 1000);
+// setTimeout(timeoutExample1, 4000);
+// setTimeout(timeoutExample2, 1000);
 
-console.log("There is no delay here");
+// console.log("There is no delay here");
+
+// console.log(location);
+
+console.log(history);
