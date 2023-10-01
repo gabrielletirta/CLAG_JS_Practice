ca4a416f3a749647cbebd0e0b86ae9972c506db4
Gabrielle Tirta <tirtagabrielle@gmail.com>
2023-10-01 23:06:21 +1100

    setTimeout practices

---
+++ api_practice.js
@@ -0,0 +1,13 @@
+// This is the examples of setTimeout
+function timeoutExample1(){
+    console.log("This messages id delayed 5 seconds");
+}
+
+function timeoutExample2(){
+    console.log("This messages id delayed 1 second");
+}
+
+setTimeout(timeoutExample1, 4000);
+setTimeout(timeoutExample2, 1000);
+
+console.log("There is no delay here");
